22b1d4988bfc76edc6ff99c566c5f8c1d78ad07a
silkov78 <piotr.silkov78@gmail.com>
2025-07-25 16:14:40 +0300

    index.php: print svir_campings.json

--- app/public/index.php
+++ app/public/index.php
@@ -1,3 +1,11 @@
 <?php
 
-phpinfo();
+declare(strict_types=1);
+
+$points = file_get_contents('../data/svir_campings.geojson');
+
+$jsonPoints = json_decode($points, true);
+
+echo "<pre>";
+print_r($jsonPoints);
+echo "</pre>";
