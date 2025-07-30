c3ba98a621a0e064830d803746e64daa6c8e22d4
silkov78 <piotr.silkov78@gmail.com>
2025-07-30 17:16:03 +0300

    show all campings in /map url

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/map', function () {
+    return \App\Models\Camping::all()->toArray();
+});
