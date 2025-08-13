c7470105b55550f394322d32a940d9b3e9ecb106
silkov78 <piotr.silkov78@gmail.com>
2025-08-13 12:01:25 +0300

    User: factory and seeder in DatabaseSeeder.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'name' => fake()->unique()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'bio' => fake()->text(55),
+            'telegram' => '@' . fake()->userName(),
         ];
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'silkov78',
+            'email' => 'piotr.silkov78@gmail.com',
+            'password' => Hash::make('silkov78'),
+            'bio' => 'Вандрую па спадчыне',
+            'telegram' => '@' . fake()->userName(),
         ]);
+
+         User::factory(10)->create();
     }
 }
