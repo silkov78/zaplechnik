94bde06b74d34e30e560458688a8caedecca0acb
silkov78 <piotr.silkov78@gmail.com>
2025-08-13 11:13:12 +0300

    user update

---
+++ database/migrations/2025_08_13_073200_update_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name', length: 50)->unique()->change();
+            $table->string('avatar')->nullable();
+            $table->string('bio')->nullable();
+            $table->string('telegram', length: 100)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+           $table->dropColumn(['avatar', 'telegram', 'bio']);
+        });
+    }
+};
