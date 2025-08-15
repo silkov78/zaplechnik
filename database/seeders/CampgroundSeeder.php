ac71b0b93f4a020c99998c8a261a03d379efc237
silkov78 <piotr.silkov78@gmail.com>
2025-08-15 13:26:39 +0300

    edit CampgroundSeeder

--- database/seeders/CampgroundSeeder.php
+++ database/seeders/CampgroundSeeder.php
@@ -2,26 +2,26 @@
 
 namespace Database\Seeders;
 
-use App\Models\Camp;
+use App\Models\Campground;
 use Clickbar\Magellan\IO\Parser\Geojson\GeojsonParser;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\File;
 
-class CampSeeder extends Seeder
+class CampgroundSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(GeojsonParser $parser): void
     {
-        $data = File::json(database_path('seeders/data/campings_seed.geojson'));
+        $data = File::json(database_path('seeders/data/campgrounds.geojson'));
 
         foreach ($data['features'] as $campData) {
             $campArray = $campData['properties'];
             $campArray['coordinates'] = $parser->parse($campData['geometry']);
 
-            Camp::create($campArray);
+            Campground::create($campArray);
         }
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            CampSeeder::class,
+            CampgroundSeeder::class,
         ]);
     }
 }
