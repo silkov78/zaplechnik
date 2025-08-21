8dd3fd933c977a17c60208ab053c59a941c5c06a
silkov78 <piotr.silkov78@gmail.com>
2025-08-21 14:10:34 +0300

    CheckIsPetyaSilkov middleware for campgrounds

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\CheckIsPetyaSilkov;
 use App\Models\Campground;
 use Illuminate\Support\Facades\Route;
 
@@ -23,4 +24,4 @@ require __DIR__.'/auth.php';
 // Campgrounds
 Route::get('/campgrounds', function () {
     return view('campgrounds.index', ['campgrounds' => Campground::all()]);
-});
+})->middleware(CheckIsPetyaSilkov::class);
