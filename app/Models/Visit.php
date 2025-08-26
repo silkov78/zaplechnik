de8997776bf2b939182d693c212b50b6a1129bb8
silkov78 <piotr.silkov78@gmail.com>
2025-08-26 14:52:22 +0300

    Visit: add HasFactory trait

--- app/Models/Visit.php
+++ app/Models/Visit.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Visit extends Model
 {
+    use HasFactory;
+
     protected $primaryKey = 'visit_id';
 
     protected $fillable = [
