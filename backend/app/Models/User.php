1caa9e6df80db2ba705df7074526c4ba1091adf7
silkov78 <piotr.silkov78@gmail.com>
2025-09-02 16:21:18 +0300

    chore: create api guard with sanctum driver

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     protected $primaryKey = 'user_id';
 
