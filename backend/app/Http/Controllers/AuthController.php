65dda0578a7ae7dcc8a7cc6f87840a3a56589eab
silkov78 <piotr.silkov78@gmail.com>
2025-09-04 16:59:46 +0300

    refactor: method register uses FormRequest and doesnt generate token

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -29,6 +29,7 @@ class AuthController extends Controller
 
     public function login(Request $request): JsonResponse
     {
+        // TODO: Refactor login method
         $data = $request->validate([
             'email' => 'required|string|email',
             'password' => 'required|string',
