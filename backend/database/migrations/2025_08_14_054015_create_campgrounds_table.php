6550a12ba30a9692bfe299a9144577fa96233e48
silkov78 <piotr.silkov78@gmail.com>
2025-09-10 14:44:15 +0300

    fix: TODO is added to create_campgrounds_table migration

--- backend/database/migrations/2025_08_14_054015_create_campgrounds_table.php
+++ backend/database/migrations/2025_08_14_054015_create_campgrounds_table.php
@@ -24,8 +24,11 @@ return new class extends Migration
             $table->string('osm_toilets', 15)->nullable();
             $table->string('osm_access', 100)->nullable();
             $table->string('firewood', 15)->nullable();
+            
+            // TODO: remove nullable from script_fields in new migration
             $table->string('script_region', 50)->nullable();
             $table->string('script_district', 50)->nullable();
+
             $table->timestamps();
         });
     }
