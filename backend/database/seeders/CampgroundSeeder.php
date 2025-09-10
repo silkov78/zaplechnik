f2d085035e7749acac3b331516da7316009189d6
silkov78 <piotr.silkov78@gmail.com>
2025-09-10 14:30:40 +0300

    refactor: edit CampgroundSeeder for new tablenames

--- backend/database/seeders/CampgroundSeeder.php
+++ backend/database/seeders/CampgroundSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Campground;
 use Clickbar\Magellan\IO\Parser\Geojson\GeojsonParser;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\File;
 
@@ -12,11 +11,32 @@ class CampgroundSeeder extends Seeder
 {
     public function run(GeojsonParser $parser): void
     {
-        $data = File::json(database_path('seeders/data/campgrounds.geojson'));
+        $seederGeoJsonPath = database_path('seeders/data/campgrounds.geojson');
+        $featuresArray = File::json($seederGeoJsonPath)['features'];
 
-        foreach ($data['features'] as $campData) {
-            $campArray = $campData['properties'];
-            $campArray['coordinates'] = $parser->parse($campData['geometry']);
+        $osmDatabaseMapping = [
+            'name' => 'osm_name',
+            'geometry' => 'osm_geometry',
+            'description' => 'osm_description',
+            'website' => 'osm_website',
+            'fee' => 'osm_fee',
+            'fireplace' => 'osm_fireplace',
+            'picnic_table' => 'osm_picnic_table',
+            'toilets' => 'osm_toilets',
+            'access' => 'osm_access',
+        ];
+
+        foreach ($featuresArray as $feature) {
+            $campArray = $feature['properties'];
+            $campArray['geometry'] = $parser->parse($feature['geometry']);
+
+            foreach ($osmDatabaseMapping as $osmName => $databaseName) {
+                if (array_key_exists($osmName, $campArray)) {
+                    $campArray[$databaseName] = $campArray[$osmName];
+
+                    unset($campArray[$osmName]);
+                }
+            }
 
             Campground::create($campArray);
         }
