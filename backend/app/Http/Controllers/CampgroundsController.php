74d497a6a1ea81e934bdf113eb30ff77eb6e006e
silkov78 <piotr.silkov78@gmail.com>
2025-09-11 11:37:58 +0300

    refactor: CampgroundsController uses trait

--- backend/app/Http/Controllers/CampgroundsController.php
+++ backend/app/Http/Controllers/CampgroundsController.php
@@ -2,43 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Concerns\GeneratesGeoJsonArray;
 use App\Models\Campground;
 use Illuminate\Http\JsonResponse;
 
 class CampgroundsController extends Controller
 {
+    use GeneratesGeoJsonArray;
+
     /**
-     * Returns campgrounds as GeoJson FeatureCollection
+     * Returns campgrounds as GeoJson featureCollection
      */
     public function index(): JsonResponse
     {
-        return response()->json([
-            'type' => 'FeatureCollection',
-            'features' => $this->getFeaturesGeoJson(),
-        ]);
-    }
+        $campgroundsArray = $this->getFeatureCollectionArray(
+            Campground::all(), 'osm_geometry'
+        );
 
-    /**
-     * Transform campgrounds to standard GeoJson format
-     *
-     * Each feature includes:
-     * - properties: all model attributes except osm_geometry
-     * - geometry: osm_geometry as standard geometry description
-     *
-     * @see https://geojson.org/ GeoJson specification
-     */
-    public function getFeaturesGeoJson(): array
-    {
-        // TODO: refactor, when all fields will be included in campgrounds table
-        return Campground::all()->map(function ($feature) {
-            $featureProperties = $feature->toArray();
-            unset($featureProperties['osm_geometry']);
-
-            return [
-                'type' => 'Feature',
-                'properties' => $featureProperties,
-                'geometry' => $feature->osm_geometry,
-            ];
-        })->toArray();
+        return response()->json($campgroundsArray);
     }
 }
