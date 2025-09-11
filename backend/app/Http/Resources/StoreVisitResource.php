9227f500a7d34171a69f23699006ac9334fbac2e
silkov78 <piotr.silkov78@gmail.com>
2025-09-11 14:40:30 +0300

    feat: StoreVisitResource for storeVisit method

---
+++ backend/app/Http/Resources/StoreVisitResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StoreVisitResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'message' => 'User successfully made a visit.',
+            'info' => [
+                'user_id' => $this->user_id,
+                'campground_id' => $this->campground_id,
+                'visit_date' => $this->visit_date->format('Y-m-d'),
+            ],
+        ];
+    }
+}
