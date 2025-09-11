4ff2f76660e8686eb1609700866f61b1dd597cc2
silkov78 <piotr.silkov78@gmail.com>
2025-09-11 11:37:13 +0300

    refactor: new trait for GeoJson generating

---
+++ backend/app/Http/Controllers/Concerns/GeneratesGeoJsonArray.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Concerns;
+
+use Illuminate\Database\Eloquent\Collection;
+
+trait GeneratesGeoJsonArray
+{
+    /**
+     * Transform geographical model collection in GeoJson featureCollection array
+     *
+     * Each feature includes:
+     * - properties: all model attributes except geometry field
+     * - geometry: geometry field in standard geometry description
+     *
+     * @param Collection $modelCollection Model collection of Geographical entity
+     * @param string $geometryField Name of geometry field in model
+     *
+     * @see https://geojson.org/ GeoJson specification
+     */
+    public function getFeatureCollectionArray(
+        Collection $modelCollection,
+        string $geometryField = 'osm_geometry',
+    ): array
+    {
+        return [
+            'type' => 'FeatureCollection',
+            'features' => $modelCollection->map(function ($feature) use ($geometryField) {
+                $featureProperties = $feature->toArray();
+                unset($featureProperties[$geometryField]);
+
+                return [
+                    'type' => 'Feature',
+                    'properties' => $featureProperties,
+                    'geometry' => $feature->osm_geometry,
+                ];
+            })->toArray(),
+        ];
+    }
+}
