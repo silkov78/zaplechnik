288173f4d3cc8c2c6e7027b0ab7ce051e46379ad
silkov78 <piotr.silkov78@gmail.com>
2025-09-15 15:56:56 +0300

    fix: custom messages for /me patch endpoint; add parameter ration in avatar

--- backend/app/Http/Requests/ProfileUpdateRequest.php
+++ backend/app/Http/Requests/ProfileUpdateRequest.php
@@ -26,11 +26,25 @@ class ProfileUpdateRequest extends FormRequest
         return [
             'name' => 'string|unique:users|max:50',
             'email' => 'email|unique:users|max:255',
-            'avatar' => 'file|image|mimes:jpeg,png,jpg,bmp,gif|max:2048',
+            'avatar' => 'file|image|mimes:jpeg,png,jpg,bmp,gif|dimensions:ratio=1/1|max:2048',
             'gender' => [new Enum(Gender::class)],
             'bio' => 'string|max:255',
             'telegram' => 'string|starts_with:@|max:100',
             'is_private' => 'boolean',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name' => 'Parameter “name” is unique. It must be a string and less than 50 characters',
+            'email' => 'Parameter “email” is unique. It must be a string and less than 255 characters',
+            'avatar' => 'Parameter “avatar” must be a square and less than 2 megabytes in size. ' .
+                        'Acceptable format is JPEG, JPG, GIF, PNG и BMP.',
+            'gender' => 'Parameter “gender” must be female or male.',
+            'bio' => 'Parameter “bio”  must be less than 255 characters.',
+            'telegram' => 'Parameter “telegram”  must must start with the character “@” and less than 100 characters.',
+            'is_private' => 'Parmeter “is_private” must be boolean.',
         ];
     }
 }
