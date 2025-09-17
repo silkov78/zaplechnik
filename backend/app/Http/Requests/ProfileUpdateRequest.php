58250e703758ad4c028c9c1338a77fc9813a5417
silkov78 <piotr.silkov78@gmail.com>
2025-09-17 13:32:20 +0300

    feat: CustomAuthMiddleware.php

--- backend/app/Http/Requests/ProfileUpdateRequest.php
+++ backend/app/Http/Requests/ProfileUpdateRequest.php
@@ -44,7 +44,7 @@ class ProfileUpdateRequest extends FormRequest
             'gender' => 'Parameter “gender” must be female or male.',
             'bio' => 'Parameter “bio”  must be less than 255 characters.',
             'telegram' => 'Parameter “telegram”  must must start with the character “@” and less than 100 characters.',
-            'is_private' => 'Parmeter “is_private” must be boolean.',
+            'is_private' => 'Parameter “is_private” must be boolean.',
         ];
     }
 }
