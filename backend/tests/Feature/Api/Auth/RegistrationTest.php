f9cd259cf6f1046b0c7684d306774aa3087f5afb
silkov78 <piotr.silkov78@gmail.com>
2025-09-19 12:53:09 +0300

    test: it rejects invalid name

--- backend/tests/Feature/Api/Auth/RegistrationTest.php
+++ backend/tests/Feature/Api/Auth/RegistrationTest.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 
 uses(RefreshDatabase::class);
 
@@ -37,5 +39,25 @@ describe('registration process', function () {
         'missing email' => [['name' => 'silkov78', 'password' => 'Silkov78']],
         'missing password' => [['name' => 'silkov78', 'password' => 'Silkov78']],
         'empty credentials' => [['name' => '', 'email' => '', 'password' => '']],
+    ]);
+
+    it('rejects invalid name', function ($invalidName) {
+        User::factory()->create([
+            'name' => 'testUser',
+        ]);
+
+        $validResponsePart = ['email' => 'piotr@example.com', 'password' => 'Silkov78'];
+        $responseArray = array_merge($validResponsePart, ['name' => $invalidName]);
+
+        $response = $this->postJson('/api/v1/register', $responseArray);
+
+        $response->assertStatus(400)->assertJsonFragment([
+            'message' => 'Invalid request',
+        ]);
+    })->with([
+        'empty name' => '',
+        'not string name' => 1,
+        '> 255 symbols name' => str_repeat('A', 300),
+        'existing name' => 'testUser',
     ]);
 });
