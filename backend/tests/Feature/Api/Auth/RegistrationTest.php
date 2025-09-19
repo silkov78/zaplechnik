6158d06461f874187b26f88145fcd43154d9ed0e
silkov78 <piotr.silkov78@gmail.com>
2025-09-19 11:16:47 +0300

    test: it rejects empty credentials

--- backend/tests/Feature/Api/Auth/RegistrationTest.php
+++ backend/tests/Feature/Api/Auth/RegistrationTest.php
@@ -24,4 +24,18 @@ describe('registration process', function () {
                 ]
             ]);
     });
+
+    it('rejects empty or partly empty credentials', function ($invalidData) {
+        $response = $this->postJson('/api/v1/register', $invalidData);
+
+        $response->assertStatus(400)->assertJsonFragment([
+            'message' => 'Invalid request',
+        ]);
+    })->with([
+        'missing credentials' => [[]],
+        'missing name' => [['email' => 'piotr@example.com', 'password' => 'Silkov78']],
+        'missing email' => [['name' => 'silkov78', 'password' => 'Silkov78']],
+        'missing password' => [['name' => 'silkov78', 'password' => 'Silkov78']],
+        'empty credentials' => [['name' => '', 'email' => '', 'password' => '']],
+    ]);
 });
