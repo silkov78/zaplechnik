cbe0e3dddc7a4b0a16c1c66021a8782acc850468
silkov78 <piotr.silkov78@gmail.com>
2025-09-24 16:59:38 +0300

    test: it revokes current token from db

--- backend/tests/Feature/Api/Auth/LogoutTest.php
+++ backend/tests/Feature/Api/Auth/LogoutTest.php
@@ -22,4 +22,26 @@ describe('logout', function () {
                 'message' => 'User logged out successfully.',
             ]);
     });
+
+    it('revokes current token from db', function () {
+        $user = User::factory()->create();
+
+        $token1 = $user->createToken('device1')->plainTextToken;
+        $token2 = $user->createToken('device2')->plainTextToken;
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token1)
+            ->postJson('/api/v1/logout');
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseMissing('personal_access_tokens', [
+            'tokenable_id' => $user->user_id,
+            'name' => 'device1',
+        ]);
+
+        $this->assertDatabaseHas('personal_access_tokens', [
+            'tokenable_id' => $user->user_id,
+            'name' => 'device2',
+        ]);
+    });
 });
