34a5adfed6ae93abd8def3ce4e636fbd573d66f4
silkov78 <piotr.silkov78@gmail.com>
2025-09-25 14:25:53 +0300

    refactor: remove redundant names from api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,35 +8,33 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function () {
     // Auth
     Route::controller(AuthController::class)->group(function () {
-        Route::post('/login','login')->name('auth.login');
-        Route::post('/register','register')->name('auth.register');
-        Route::post('/logout','logout')
-            ->name('auth.logout')
-            ->middleware('auth:sanctum');
+        Route::post('/login','login');
+        Route::post('/register','register');
+        Route::post('/logout','logout')->middleware('auth:sanctum');
     });
 
     // Profile
-    Route::controller(ProfileController::class)->prefix('me')->name('me')->group(function () {
-        Route::middleware('auth:sanctum')->group(function () {
+    Route::controller(ProfileController::class)
+        ->middleware('auth:sanctum')
+        ->prefix('me')
+        ->group(function () {
             // Profile data
-            Route::get('/', 'show')->name('show');
-            Route::patch('/', 'update')->name('update');
-            Route::delete('/', 'destroy')->name('destroy');
+            Route::get('/', 'show');
+            Route::patch('/', 'update');
+            Route::delete('/', 'destroy');
 
-            // Visits
-            Route::post('/visits', 'storeVisit')->name('visits.store');
-            Route::delete('/visits', 'destroyVisit')->name('visits.destroy');
-        });
+            // Profile visits
+            Route::post('/visits', 'storeVisit');
+            Route::delete('/visits', 'destroyVisit');
     });
 
     // Campgrounds
     Route::controller(CampgroundsController::class)->group(function () {
-        Route::get('/campgrounds', 'index')->name('campgrounds.index');
+        // All campgrounds
+        Route::get('/campgrounds', 'index');
 
         // Visited campgrounds
-        Route::get(
-            '/me/visited-campgrounds',
-            'visitedCampgrounds'
-        )->middleware('auth:sanctum');
+        Route::get('/me/visited-campgrounds', 'visitedCampgrounds')
+            ->middleware('auth:sanctum');
     });
 });
