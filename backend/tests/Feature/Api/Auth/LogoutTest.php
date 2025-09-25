0727f13cc380ea3d9be75d9765f6259308737de0
silkov78 <piotr.silkov78@gmail.com>
2025-09-25 14:07:30 +0300

    test: beforeEach in LogoutTest.php

--- backend/tests/Feature/Api/Auth/LogoutTest.php
+++ backend/tests/Feature/Api/Auth/LogoutTest.php
@@ -6,10 +6,13 @@ use Laravel\Sanctum\Sanctum;
 
 uses(RefreshDatabase::class);
 
+beforeEach(function () {
+    $this->user = User::factory()->create();
+});
+
 describe('logout', function () {
     it('returns correct message after logout', function () {
-        $user = User::factory()->create();
-        Sanctum::actingAs($user);
+        Sanctum::actingAs($this->user);
 
         $response = $this->postJson('/api/v1/logout');
 
@@ -24,10 +27,8 @@ describe('logout', function () {
     });
 
     it('revokes current token from db', function () {
-        $user = User::factory()->create();
-
-        $token1 = $user->createToken('device1')->plainTextToken;
-        $token2 = $user->createToken('device2')->plainTextToken;
+        $token1 = $this->user->createToken('device1')->plainTextToken;
+        $token2 = $this->user->createToken('device2')->plainTextToken;
 
         $response = $this->withHeader('Authorization', 'Bearer ' . $token1)
             ->postJson('/api/v1/logout');
@@ -35,12 +36,12 @@ describe('logout', function () {
         $response->assertStatus(200);
 
         $this->assertDatabaseMissing('personal_access_tokens', [
-            'tokenable_id' => $user->user_id,
+            'tokenable_id' => $this->user->user_id,
             'name' => 'device1',
         ]);
 
         $this->assertDatabaseHas('personal_access_tokens', [
-            'tokenable_id' => $user->user_id,
+            'tokenable_id' => $this->user->user_id,
             'name' => 'device2',
         ]);
     });
