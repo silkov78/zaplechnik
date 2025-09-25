b1b48908001353dc00c778f9e2a72bd82b05b926
silkov78 <piotr.silkov78@gmail.com>
2025-09-25 13:05:22 +0300

    test: 'it returns campgrounds visited by user' test

--- backend/tests/Feature/Api/CampgroundsTest.php
+++ backend/tests/Feature/Api/CampgroundsTest.php
@@ -1,15 +1,50 @@
 <?php
 
 use App\Models\Campground;
+use App\Models\Visit;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Sanctum\Sanctum;
 
 uses(RefreshDatabase::class);
 
 describe('campgrounds', function () {
-    it('returns geojson feature collection', function () {
+    it('returns campgrounds feature collection', function () {
         Campground::factory(10)->create();
 
         $response = $this->getJson('/api/v1/campgrounds');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'type',
+                'features' => [
+                    [
+                        'type',
+                        'properties' => [],
+                        'geometry' => [
+                            'type',
+                            'coordinates' => [],
+                        ],
+                    ]
+                ],
+            ])
+            ->assertJsonFragment([
+                'type' => 'FeatureCollection',
+            ]);
+    });
+
+    it('returns campgrounds visited by user', function () {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        Campground::factory()->create();
+
+        Visit::factory()->create([
+            'user_id' => $user->user_id,
+            'campground_id' => 13,
+        ]);
+
+        $response = $this->getJson('/api/v1/me/visited-campgrounds');
 
         $response->assertStatus(200)
             ->assertJsonStructure([
