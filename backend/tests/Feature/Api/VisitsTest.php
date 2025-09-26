5655adf21fc3d39c0b761b9342d7bb37eaf3760c
silkov78 <piotr.silkov78@gmail.com>
2025-09-26 13:33:32 +0300

    test: visits: it rejects invalid campground_id

--- backend/tests/Feature/Api/VisitsTest.php
+++ backend/tests/Feature/Api/VisitsTest.php
@@ -19,4 +19,34 @@ describe('visits', function () {
         $response = $this->postJson('/api/v1/visits');
         $response->assertStatus(401);
     });
+
+    it('rejects invalid campground_id', function ($invalidCampgroundId) {
+        Sanctum::actingAs($this->user);
+
+        $data = [
+            'campground_id' => $invalidCampgroundId,
+            'visit_date' => '2025-01-01',
+        ];
+
+        $response = $this->postJson('/api/v1/visits', $data);
+
+        dump($response->json());
+
+        $response->assertStatus(422)
+            ->assertJsonStructure([
+                'message',
+                'errors' => [
+                    'campground_id' => [
+                        'code',
+                        'message',
+                    ],
+                ],
+            ]);
+    })->with([
+        'empty campground_id' => '',
+        'string campground_id' => 'fdsfsadf',
+        'float campground_id' => 2.0,
+        'negative campground_id' => -2,
+        'zero campground_id' => 0,
+    ]);
 });
