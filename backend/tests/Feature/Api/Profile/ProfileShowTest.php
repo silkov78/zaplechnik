33b5ba184f8b4ca288cb56807728a763b1892db9
silkov78 <piotr.silkov78@gmail.com>
2025-09-28 16:34:55 +0300

    test: profile show: 'it rejects not-authenticated user'

---
+++ backend/tests/Feature/Api/Profile/ProfileShowTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+beforeEach(function () {
+    $this->user = User::factory()->create([
+        'name' => 'testUser',
+    ]);
+});
+
+describe('profile: store', function () {
+    it('rejects not-authenticated user', function () {
+        $this->getJson('/api/v1/me')->assertStatus(401);
+    });
+});
