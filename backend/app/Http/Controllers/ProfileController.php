c275ea7263db617ed3a1e8852ca997f14d705027
silkov78 <piotr.silkov78@gmail.com>
2025-09-28 16:35:27 +0300

    refactor: remove redundant methods from ProfileController.php

--- backend/app/Http/Controllers/ProfileController.php
+++ backend/app/Http/Controllers/ProfileController.php
@@ -12,14 +12,13 @@ use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
 {
-    public function show(Request $request): ProfileResource
+    public function show(Request $request): JsonResponse
     {
-        return new ProfileResource($request->user());
+        return response()->json([]);
     }
 
     /**
-     * Updates user's data
-     * Handles attached avatar photo
+     * Updates user's data. Handles attached avatar photo.
      */
     public function update(ProfileUpdateRequest $request): JsonResponse
     {
@@ -49,8 +48,7 @@ class ProfileController extends Controller
     }
 
     /**
-     * Deletes user's data (account)
-     * Delete avatar photo from storage
+     * Deletes user's data (account). Delete avatar photo from storage.
      */
     public function destroy(Request $request): JsonResponse
     {
@@ -68,57 +66,6 @@ class ProfileController extends Controller
         return response()->json([
             'message' => 'User successfully deleted an account.',
             'info' => ['user_id' => $userId],
-        ]);
-    }
-
-    /**
-     * Stores new visit record
-     * Each visit has unique pair of user_id and campground_id
-     */
-    public function storeVisit(Request $request): StoreVisitResource|JsonResponse
-    {
-        $data = $request->validate([
-            'campground_id' => 'required|integer|exists:campgrounds',
-            'visit_date' => 'date:Y-m-d',
-        ]);
-
-        $data['user_id'] = $request->user()->user_id;
-
-        $visit = Visit::create($data);
-
-        return new StoreVisitResource($visit);
-    }
-
-    /**
-     * Destroys visit record
-     * Accepts required campground_id from query string
-     */
-    public function destroyVisit(Request $request): JsonResponse
-    {
-        $userId = $request->user()->user_id;
-        $query = $request->validate([
-            'campground_id' => 'required|integer|exists:campgrounds',
-        ]);
-
-        $visit = Visit::where([
-            'user_id' => $userId,
-            'campground_id' => $query['campground_id'],
-        ]);
-
-        if (!$visit->exists()) {
-            return response()->json([
-                'message' => 'Visit with provided user_id and campground_id not found.',
-            ]);
-        }
-
-        $visit->delete();
-
-        return response()->json([
-            'message' => 'User successfully deleted a visit',
-            'info' => [
-                'user_id' => $userId,
-                'campground_id' => $query['campground_id'],
-            ],
         ]);
     }
 }
