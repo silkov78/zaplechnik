c9f6771640178d46e2ccc9def3053b2e6c0d6de5
silkov78 <piotr.silkov78@gmail.com>
2025-09-29 13:11:14 +0300

    feat: profile update: avatar field validation

--- backend/app/Http/Requests/ProfileUpdateRequest.php
+++ backend/app/Http/Requests/ProfileUpdateRequest.php
@@ -18,7 +18,7 @@ class ProfileUpdateRequest extends FormRequest
         return [
             'name' => 'string|max:50|min:1',
             'email' => 'string|email|max:255',
-//            'avatar' => 'file|image|mimes:jpeg,png,jpg,bmp,gif|dimensions:ratio=1/1|max:2048',
+            'avatar' => 'file|image|mimes:jpeg,png,jpg,bmp,gif|dimensions:ratio=1/1|max:2048',
             'gender' => Rule::in(Gender::values()),
             'bio' => 'string|max:255',
             'telegram' => 'string|starts_with:@|max:100',
