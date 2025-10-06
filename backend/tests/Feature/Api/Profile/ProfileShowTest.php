734b0a39e5dcf90c77eddf09e0b7b1b32fb7c329
silkov78 <piotr.silkov78@gmail.com>
2025-10-06 15:29:32 +0300

    feat: 'medium' rate limit for 'profile show' endpoint

--- backend/tests/Feature/Api/Profile/ProfileShowTest.php
+++ backend/tests/Feature/Api/Profile/ProfileShowTest.php
@@ -11,11 +11,7 @@ beforeEach(function () {
     ]);
 });
 
-describe('profile: store', function () {
-    it('rejects not-authenticated user', function () {
-        $this->getJson('/api/v1/me')->assertStatus(401);
-    });
-
+describe('profile: show', function () {
     it('shows profile data successfully', function () {
         $this->actingAs($this->user);
 
@@ -42,5 +38,24 @@ describe('profile: store', function () {
         $this->assertStringContainsString(
             'images/default-avatar.png', $response['data']['avatarUrl']
         );
+    });
+
+    it('rejects not-authenticated user', function () {
+        $this->getJson('/api/v1/me')->assertStatus(401);
+    });
+
+    it('rejects requests by medium rate limit (40)', function () {
+        $this->actingAs($this->user);
+
+        for ($i = 0; $i < 40; $i++) {
+            $response = $this->getJson('/api/v1/me');
+            expect($response->status())->not()->toBe(429);
+        }
+
+        $response = $this->getJson('/api/v1/me');
+
+        $response->assertStatus(429)
+            ->assertJsonStructure(['message', 'errors' => ['rate-limit' => ['code', 'message']]])
+            ->assertJsonFragment(['code' => 'rate-limit']);
     });
 });
