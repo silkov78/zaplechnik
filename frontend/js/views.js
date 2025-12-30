e79248796d87224908bd05fcd440d5121c9da228
silkov78 <piotr.silkov78@gmail.com>
2025-12-30 11:09:09 +0300

    fix: use field name from database (prefixes osm_ and script_)

--- frontend/js/views.js
+++ frontend/js/views.js
@@ -62,26 +62,28 @@ export const views = {
   },
 
   camp: (data) => {
-    const regionTag = data.properties['region:be'] ? `#${data.properties['region:be'].replace(' вобласць', '').replace(' вобл.', '')}` : '';
-    const districtTag = data.properties['district:be'] ? `#${data.properties['district:be'].replace(' раён', '').replace(' р-н', '')}` : '';
+    console.log(data.properties);
+
+    const regionTag = data.properties.script_region ? `#${data.properties.script_region.replace(' вобласць', '').replace(' вобл.', '')}` : '';
+    const districtTag = data.properties.script_district ? `#${data.properties.script_district.replace(' раён', '').replace(' р-н', '')}` : '';
     
     return `
-    <h2 class="sidebar-title">${data.properties.name || 'Без назвы'}</h2>
+    <h2 class="sidebar-title">${data.properties.osm_name || 'Без назвы'}</h2>
     
-    <img src="${data.properties.image || 'test_image.png'}" alt="Фото места" class="sidebar-image">
+    <img src="${data.properties.osm_image || 'test_image.png'}" alt="Фото места" class="sidebar-image">
     <p class="sidebar-coords">${data.geometry.coordinates[1].toFixed(6)}, ${data.geometry.coordinates[0].toFixed(6)}</p>
     <button id="close-sidebar-btn">&times;</button>
 
     <ul class="sidebar-details">
-      ${data.properties.website ? `<li><strong>Вэб-сайт:</strong> <a href="${data.properties.website}" target="_blank">тык</a></li>` : ''}
-      ${data.properties.fee ? `<li><strong>Інфармацыя пра плату:</strong> ${data.properties.fee === 'yes' ? 'платна' : 'бясплатна'}</li>` : ''}
-      ${data.properties.fireplace ? `<li><strong>Абсталяваныя месцы для развядзення агню:</strong> ${data.properties.fireplace === 'yes' ? 'так' : 'не'}</li>` : ''}
-      ${data.properties.table ? `<li><strong>Наяўнасць стала:</strong> ${data.properties.picnic_table === 'yes' ? 'так' : 'не'}</li>` : ''}
-      ${data.properties.toilet ? `<li><strong>Наяўнасць туалета:</strong> ${data.properties.toilet === 'yes' ? 'так' : 'не'}</li>` : ''}
-      ${data.properties.access ? `<li><strong>Даступнасць:</strong> ${data.properties.access}</li>` : ''} 
-      ${data.properties.last_update ? `<li><strong>Дата апошняй актуалізацыі дадзеных стаянкі:</strong> ${data.properties.last_update}</li>` : ''}
-      ${data.properties.description ? `<li><strong>Апісанне:</strong> ${data.properties.description}</li>` : ''}
-      <li><strong>ОSМ:</strong> <a href="https://www.openstreetmap.org/${data.properties['@id']}" target="_blank">лінка</a></li>      
+      ${data.properties.osm_website ? `<li><strong>Вэб-сайт:</strong> <a href="${data.properties.osm_website}" target="_blank">тык</a></li>` : ''}
+      ${data.properties.osm_fee ? `<li><strong>Інфармацыя пра плату:</strong> ${data.properties.osm_fee === 'yes' ? 'платна' : 'бясплатна'}</li>` : ''}
+      ${data.properties.osm_fireplace ? `<li><strong>Абсталяваныя месцы для развядзення агню:</strong> ${data.properties.osm_fireplace === 'yes' ? 'так' : 'не'}</li>` : ''}
+      ${data.properties.osm_table ? `<li><strong>Наяўнасць стала:</strong> ${data.properties.osm_picnic_table === 'yes' ? 'так' : 'не'}</li>` : ''}
+      ${data.properties.osm_toilet ? `<li><strong>Наяўнасць туалета:</strong> ${data.properties.osm_osm_toilet === 'yes' ? 'так' : 'не'}</li>` : ''}
+      ${data.properties.osm_access ? `<li><strong>Даступнасць:</strong> ${data.properties.osm_access}</li>` : ''} 
+      ${data.properties.osm_last_update ? `<li><strong>Дата апошняй актуалізацыі дадзеных стаянкі:</strong> ${data.properties.osm_last_update}</li>` : ''}
+      ${data.properties.osm_description ? `<li><strong>Апісанне:</strong> ${data.properties.osm_description}</li>` : ''}
+      <li><strong>ОSМ:</strong> <a href="https://www.openstreetmap.org/${data.properties.osm_id}" target="_blank">лінка</a></li>      
     </ul>
 
     <div class="hashtags">
